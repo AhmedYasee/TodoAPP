6ac5a5b26f59b655a01cdb0613a8794b95766c5d
AhmedYasee <eng.ahmedyaseen4@gmail.com>
2024-04-26 23:56:11 +0300

    Add project files.

---
+++ TodoAPP/Context/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAPP.Models;
+
+namespace TodoAPP.Context
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=Todo;Trusted_connection=true;Encrypt=false");
+        }
+    }
+}
